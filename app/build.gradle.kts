44e0fc54ab6ca11ee98c78a3a8179dd8a8993618
kiterai <kiterai@foolslab.net>
2024-07-28 13:41:39 +0900

    remove tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,4 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.games.activity)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
